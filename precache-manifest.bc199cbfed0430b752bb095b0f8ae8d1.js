71fd4a42214af328182657ef7a7f6ef6760e9e0f
Alexandra Boyko <Alexboyko04@gmail.com>
2019-12-13 16:41:51 +0200

    Updates

--- precache-manifest.bc199cbfed0430b752bb095b0f8ae8d1.js
+++ precache-manifest.bc199cbfed0430b752bb095b0f8ae8d1.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "846a192574418b197737554b48685981",
+    "revision": "a235f88c48fc053b953502aa490dba12",
     "url": "/react_todo-app/index.html"
   },
   {
-    "revision": "9e0d7b0aed66ed61933b",
+    "revision": "24f21447d5f324688b8b",
     "url": "/react_todo-app/static/css/main.a8e92d56.chunk.css"
   },
   {
-    "revision": "2315f74ce3c99316133d",
-    "url": "/react_todo-app/static/js/2.3310f33a.chunk.js"
+    "revision": "b829692f51f58c98b1bd",
+    "url": "/react_todo-app/static/js/2.d29b3332.chunk.js"
   },
   {
-    "revision": "9e0d7b0aed66ed61933b",
-    "url": "/react_todo-app/static/js/main.b6ea25b8.chunk.js"
+    "revision": "24f21447d5f324688b8b",
+    "url": "/react_todo-app/static/js/main.c781ba52.chunk.js"
   },
   {
     "revision": "51af3b3c9c5f8ccb1c53",
